1e2b021c1316a1784f92b1c0c40fb63800e09522
Jesse Storimer <jstorimer@gmail.com>
2013-03-07 16:57:50 -0500

    Hack Qless.worker_name for threaded usage

--- lib/qless/threaded_worker.rb
+++ lib/qless/threaded_worker.rb
@@ -1,2 +1,11 @@
 require 'qless/threaded_worker/version'
 
+# Hack the default worker_name to include the current Thread id
+# otherwise it won't be unique across worker threads
+module Qless
+  def worker_name
+    preamble ||= [Socket.gethostname, Process.pid.to_s].join('-')
+    [preamble, Thread.current.object_id].join('-')
+  end
+end
+
